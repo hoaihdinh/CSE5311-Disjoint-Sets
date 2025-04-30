6e9ad8b2fd579c5564fe4644dbfe7f2bc4aa33bb
hoaihdinh <hoai.dinh.cs@gmail.com>
2025-04-29 20:48:21 -0500

    add some comments

--- implementation/src/union_find_tree.h
+++ implementation/src/union_find_tree.h
@@ -10,12 +10,18 @@ struct __uf_node_t__ {
 
 uf_node_t* make_set(int data);
 
+// basic union operation
 uf_node_t* union_sets(uf_node_t* x, uf_node_t* y);
+
+// basic find operation
 uf_node_t* find_set(uf_node_t* x);
 
+// union function that utilizes path compression
 uf_node_t* union_sets_path_compression(uf_node_t* x, uf_node_t* y);
+// find function that utilizes path compression 
 uf_node_t* find_set_path_compression(uf_node_t* x);
 
+// union function that utilizes path compression and union by rank
 uf_node_t* union_sets_pcr(uf_node_t* x, uf_node_t* y);
 
 #endif
